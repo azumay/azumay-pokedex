811c8c8abdb0f7953ad9e579fb3d0cc77be1ab38
dduran <denis.duran.44@gmail.com>
2022-04-28 23:59:07 +0200

    v2.01 pokemon entries using Context Charging parameters correctly

--- src/components/model/PokemonEntry.js
+++ src/components/model/PokemonEntry.js
@@ -5,15 +5,31 @@ import PokemonList from "./PokemonList";
 
 function PokemonEntry() {
     const {pokemon} = useContext(PokemonContext);
+    const [currentPokemon, setCurrentPokemon] = useState([]);
+    const getData = (id) => {
+        const url="https://pokeapi.co/api/v2/pokemon/"+id;
+        fetchData(url)
+            .then(function (data) {
+                setCurrentPokemon([data]);
+            })
+            .catch(function (err) {
+                console.error(err);
+            });
+    }
     const upperCase = (text) =>{
         return text.charAt(0).toUpperCase() + text.slice(1)
     };
+    useEffect(() => {
+        getData(pokemon.id);
+    }, [pokemon]);
     return (
         <div id="currentPokemon">
            <div id="title">
-               {"#"+pokemon.id+" "+upperCase(pokemon.name)}
-
+               {pokemon===[]?"#1 Bulbasaur":"#"+pokemon.id+" "+pokemon.name}
            </div>
+            <div id="image">
+                <img src={currentPokemon[0].sprites.other["official-artwork"].front_default} style={{width: "300px"}}/>
+            </div>
         </div>
     );
 }
